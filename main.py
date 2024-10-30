c8f7d82a9e97001c7e76e46f6904ccc943153ebd
Dyeadal <dyeadal@proton.me>
2024-10-30 16:32:04 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Created by dyeadal
+# GNU General Public License 3.0
+import requests
+import hashlib
+import os
+
+# Function to download possible new daily file
+
+# Function to create hash of file
+
+# Function to compare hashes of multiple files
+
+# Function to display differences
+
+# Function to show line ADDED differences (exludes removed older entries minimizing data usage)
+
+# Function to create hyperlink of single domain/URL update of difference
+
+# Function to delete file
+
+# Function to rename file
+
+# Main Function
